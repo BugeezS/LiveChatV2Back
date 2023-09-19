4d344588e904b626593ebc582af95a75a8dd0bbb
Bruno <brunolb.lafont@gmail.com>
2023-09-19 09:23:41 +0200

    init commit

---
+++ src/main/java/com/example/livechatv2back/LiveChatV2BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.livechatv2back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveChatV2BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveChatV2BackApplication.class, args);
+    }
+
+}
